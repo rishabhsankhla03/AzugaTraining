dfdc6d68fd7ca0651c4e3ea19348ac3eba5a8d96
rishabhsankhla03 <rishabh.sankhla20@gmail.com>
2022-09-22 19:43:05 +0530

    weather api with logging code

--- day1/ApiW.java
+++ day1/ApiW.java
@@ -9,10 +9,8 @@ package com.training.day1;
 
 
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.CDL;
 import org.json.JSONArray;
 import org.json.JSONTokener;
@@ -26,25 +24,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ApiW {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    private static final Logger log= LogManager.getLogger(ApiW.class);
 
-        /**
+    public static void main(String[] args) throws IOException, InterruptedException {
+    log.info("fetching data from weather api");
+        /*
          * accessing response body and storing the response body into string "sb".
          */
 
         String[] city = {"tokyo","sydney","delhi","dubai","milan","madrid","berlin","amsterdam","washington","vancouver","Chicago","rome","lyon","turin"};
-        String sb3 = null;
+
+        log.info("fetching details of each city from weather api url");
+
+        String sb3;
         StringBuilder sting = new StringBuilder();
         sting.append("[");
+
+        log.warn("the api url can fail, check the key");
+
         for(int i=0;i< city.length;i++) {
+            log.debug("taking data of "+city[i]+ " from weather api url");
 
             //"city" string array that will take all the given city names
+
             var url = "http://api.weatherapi.com/v1/current.json?key=2d14c37a9ef84ef6ab980624221809&q=" + city[i] + "&aqi=yes";
 //            var url = "http://api.weatherapi.com/v1/current.json?key=bd4fc916e20c46f886b05941221309&q=" + city[i] + "&aqi=yes";
 
-            /**
-             * sending reequest to server
-             * based on request web brower responding to the client
+            /*
+             * sending request to server
+             * based on request web browser responding to the client
              */
 
             var request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
@@ -73,6 +81,7 @@ public class ApiW {
 
             // making a json file.
         }
+        log.info("json file fetched and now converting to csv file ");
         sting.append("]");
         try (FileWriter fw = new FileWriter("/Users/azuga/Desktop/weather.json")) {
             fw.write(sting.toString());
@@ -80,6 +89,7 @@ public class ApiW {
         } catch (Exception e) {
             System.out.println("an error occurred while creating file");
         }
+        log.info("Got response from the server-URL");
 
         // making tokens of the json Array
         //JSONTokener takes a source string and extracts character from it
@@ -91,6 +101,8 @@ public class ApiW {
         StringBuilder csv = new StringBuilder();
         csv.append(CDL.toString(jsonArray));
 
+        log.info("printing csv file from the data of json");
+
         //fetching data of json file to csv file and printing the same
 
         try {
@@ -100,5 +112,7 @@ public class ApiW {
             e.printStackTrace();
         }
         System.out.println("task completed");
+        log.info("task completed");
+//        log.warn("check for the data connection if failed");
     }
 }
