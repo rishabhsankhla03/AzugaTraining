afd92fcca2d57729fa4574e10d588e679c9a0550
rishabhsankhla03 <113594167+rishabhsankhla03@users.noreply.github.com>
2022-11-18 15:05:14 +0530

    Commit 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     public static void main(String args[]){
         System.out.println("Hello Java");
-        System.out.println("This is Rishabh!!!");
+        System.out.println("This is Rishabh Sankhla!!!");
     }
 }
